461513441e1535162ceee15828b9110baba0320f
Patnekuv Ros (Junhyeop Lim) <limjunehyeop@gmail.com>
2022-01-30 09:10:13 +0900

    Update dday.js

--- dday.js
+++ dday.js
@@ -1,12 +1,12 @@
-var dday = new Date("February 07, 2022, 8:30:00").getTime();
+var dday = new Date("January 30, 2022, 9:10:00").getTime();
 
 setInterval(function () {
 	var today = new Date().getTime();
 	var gap = dday - today;
-	var d = Math.ceil(gap / (1000 * 60 * 60 * 24));
-	var h = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var m = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60));
-	var s = Math.ceil((gap % (1000 * 60)) / 1000);
+	var d = Math.ceil(gap / (1000 * 60 * 60 * 24) - 1);
+	var h = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) - 1);
+	var m = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60) - 1);
+	var s = Math.ceil((gap % (1000 * 60)) / 1000 - 1);
 	// document.getElementById("count").innerHTML =
 	// 	"개학까지 " +
 	// 	d +
