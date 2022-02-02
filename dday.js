7913a607070b4323eeccb482637d4642a90893c7
Patnekuv Ros (Junhyeop Lim) <limjunehyeop@gmail.com>
2022-02-02 10:25:36 +0900

    Update dday.js

--- dday.js
+++ dday.js
@@ -16,20 +16,20 @@ setInterval(function () {
 	}
 }, 1000);
 
-var dday2 = new Date("Febuary 17, 2022, 0:00:00").getTime();
+// var dday2 = new Date("Febuary 17, 2022, 0:00:00").getTime();
 
-setInterval(function () {
-	var today = new Date().getTime();
-	var gap = dday2 - today;
-	var d = Math.ceil(gap / (1000 * 60 * 60 * 24) - 1);
-	var h = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) - 1);
-	var m = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60) - 1);
-	var s = Math.ceil((gap % (1000 * 60)) / 1000 - 1);
-	if (gap <= 0) {
-		document.getElementById("count2").innerHTML = "생일입니다.";
-	} else {
-		document.getElementById(
-			"count2"
-		).innerHTML = `생일까지 ${d}일 ${h}시간 ${m}분 ${s}초 남았습니다.`;
-	}
-}, 1000);
+// setInterval(function () {
+// 	var today = new Date().getTime();
+// 	var gap = dday2 - today;
+// 	var d = Math.ceil(gap / (1000 * 60 * 60 * 24) - 1);
+// 	var h = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) - 1);
+// 	var m = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60) - 1);
+// 	var s = Math.ceil((gap % (1000 * 60)) / 1000 - 1);
+// 	if (gap <= 0) {
+// 		document.getElementById("count2").innerHTML = "생일입니다.";
+// 	} else {
+// 		document.getElementById(
+// 			"count2"
+// 		).innerHTML = `생일까지 ${d}일 ${h}시간 ${m}분 ${s}초 남았습니다.`;
+// 	}
+// }, 1000);
