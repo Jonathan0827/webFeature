c718c898d0c8bdf797ce1a4dc294775017d59c92
Patnekuv Ros (Junhyeop Lim) <limjunehyeop@gmail.com>
2022-02-12 17:21:45 +0900

    minor update

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ function colorScheme(i) {
 	if (cmode === "day") {
 		document.querySelector("body").style.backgroundColor = "black";
 		document.querySelector("body").style.color = "white";
-		document.querySelector("a").style.color = "white";
 		document.querySelector("body").dataset.mode = "night";
 		cmode = "night";
 		i.value = "day";
@@ -13,7 +12,6 @@ function colorScheme(i) {
 		document.querySelector("body").style.backgroundColor = "white";
 		document.querySelector("body").style.color = "black";
 		document.querySelector("body").dataset.mode = "day";
-		document.querySelector("a").style.color = "black";
 		cmode = "day";
 		i.value = "night";
 	}
