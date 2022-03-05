ff8929a122931f750c3b2992f96600279fd8a1ca
Patnekuv Ros (Junhyeop Lim) <limjunehyeop@gmail.com>
2022-03-05 15:51:12 +0900

    APPLE EVENT

--- dday.js
+++ dday.js
@@ -1,4 +1,8 @@
-var dday = new Date("Febuary 17, 2022, 00:00:00").getTime();
+<<<<<<< HEAD
+var dday = new Date("March 9, 2022, 00:03:00").getTime();
+=======
+var dday = new Date("March 02, 2022, 08:30:00").getTime();
+>>>>>>> 42c16875d8617c2577769fb85c1fea8bd3923370
 
 setInterval(function () {
 	var today = new Date().getTime();
@@ -8,10 +12,14 @@ setInterval(function () {
 	var m = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60) - 1);
 	var s = Math.ceil((gap % (1000 * 60)) / 1000 - 1);
 	if (gap <= 0) {
-		document.getElementById("count").innerHTML = "생일입니다.";
+		document.getElementById("count").innerHTML = "";
 	} else {
 		document.getElementById(
 			"count"
-		).innerHTML = `생일까지 ${d}일 ${h}시간 ${m}분 ${s}초 남았습니다.`;
+<<<<<<< HEAD
+		).innerHTML = `Apple Peek Performance까지 ${d}일 ${h}시간 ${m}분 ${s}초 남았습니다.`;
+=======
+		).innerHTML = `개학까지 ${d}일 ${h}시간 ${m}분 ${s}초 남았습니다.`;
+>>>>>>> 42c16875d8617c2577769fb85c1fea8bd3923370
 	}
 }, 1000);
